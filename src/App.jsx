21e4c18dda55577f5e819deb6437cb05acf29852
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-06-03 18:17:00 +0900

    Initial commit with Vite and React setup

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import Pokemon from './Pokemon';
+import './App.css';
+
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const promises = [];
+      for (let i = 1; i <= 1025; i++) { 
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then(res => res.json()));
+      }
+
+      const results = await Promise.all(promises);
+      const pokemonData = results.map(data => ({
+        name: data.name,
+        id: data.id,
+        image: data.sprites.front_default,
+        type: data.types.map(type => type.type.name).join(', ')
+      }));
+      setPokemon(pokemonData);
+    };
+    
+    fetchPokemon();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>ポケモン図鑑</h1>
+      </header>
+      <Pokemon pokemon={pokemon} />
+    </div>
+  );
+}
+export default App;
---
+++ src/Pokemon.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Card';
+
+const Pokemon = ({ pokemon }) => {
+return (
+    <div className="pokemon-list">
+    {pokemon.map(pokemon => (
+        <Card key={pokemon.id} pokemon={pokemon} />
+    ))}
+    </div>
+);
+};
+
+export default Pokemon;
